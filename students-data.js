916406662b5cfb0ac1d65f93331bd7d0e275edda
Beera Ramesh <99112355+rameshbeera@users.noreply.github.com>
2022-02-06 11:45:58 +0530

    Create students-data.js

---
+++ students-data.js
@@ -0,0 +1,42 @@
+[
+  {
+    "id": 1,
+    "fullName": "Jenny Chan",
+    "book name":"NS Material"
+    "address": "3 waterfoot road",
+    "phoneNumber": "333-962-7516",
+    "email": "jenny.chan@email.com"
+  },
+  {
+    "id": 2,
+    "fullName": "Jessica warren",
+    "book name":"Science Material"
+    "address": "4 tall town",
+    "phoneNumber": "011-211-7516",
+    "email": "jessica.warren@email.com"
+  },
+  {
+    "id": 3,
+    "fullName": "Tony Frank",
+    "book name":"English Material"
+    "address": "11 lesly road",
+    "phoneNumber": "788-962-7516",
+    "email": "tony.frank@email.com"
+  },
+  {
+    "id": 4,
+    "fullName": "Jeremy Clark",
+    "book name":"Maths Material"
+    "address": "333 miltown manor",
+    "phoneNumber": "011-962-111",
+    "email": "jeremy.clark@email.com"
+  },
+  {
+    "id": 5,
+    "fullName": "Raymond Edwards",
+    "book name":"Social Material"
+    "address": "99 blue acres",
+    "phoneNumber": "3231-962-7516",
+    "email": "raymon.edwards@email.com"
+  }
+]
